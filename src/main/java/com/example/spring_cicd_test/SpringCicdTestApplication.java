93a6ebf953292cd2923738dadd2489bc6dca95d6
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-26 18:43:41 +0900

    init

---
+++ src/main/java/com/example/spring_cicd_test/SpringCicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cicd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_cicd_test/SpringCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_cicd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
