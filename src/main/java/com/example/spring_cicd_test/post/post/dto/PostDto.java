296ec83c7f967b6d3feb6003060b43502fffe6fd
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-27 23:31:51 +0900

    [feat] #6 create PostDto

--- src/main/java/com/example/spring_cicd_test/post/post/dto/PostDto.java
+++ src/main/java/com/example/spring_cicd_test/post/post/dto/PostDto.java
@@ -1,4 +1,14 @@
 package com.example.spring_cicd_test.post.post.dto;
 
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDto {
+    private Long id;
+    private String title;
+    private String body;
 }
