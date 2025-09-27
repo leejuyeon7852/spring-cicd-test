25a298cc7495573703c1029ed3385a52f8a1a996
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-27 23:53:50 +0900

    [feat] #6 create PostController

--- src/main/java/com/example/spring_cicd_test/post/post/controller/PostController.java
+++ src/main/java/com/example/spring_cicd_test/post/post/controller/PostController.java
@@ -1,4 +1,32 @@
 package com.example.spring_cicd_test.post.post.controller;
 
+import com.example.spring_cicd_test.post.post.dto.PostDto;
+import com.example.spring_cicd_test.post.post.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/post")
 public class PostController {
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<PostDto>> getAllPosts(){
+        List<PostDto> allPosts = postService.getAllPosts();
+
+        return ResponseEntity.ok(allPosts);
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(
+            @RequestBody PostDto dto
+    ){
+        PostDto saved = postService.createPost(dto);
+
+        return ResponseEntity.ok(saved);
+    }
 }
