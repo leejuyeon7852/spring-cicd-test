de7d9e9cd9d628e563f5c4f6c9126b42581b9d91
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-28 00:08:11 +0900

    [feat] #6 initData

---
+++ src/main/java/com/example/spring_cicd_test/global/initData/PostInit.java
@@ -0,0 +1,39 @@
+package com.example.spring_cicd_test.global.initData;
+
+import com.example.spring_cicd_test.post.post.dto.PostDto;
+import com.example.spring_cicd_test.post.post.entity.Post;
+import com.example.spring_cicd_test.post.post.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import java.util.List;
+
+@Configuration
+@AllArgsConstructor
+public class PostInit {
+    private final PostRepository postRepository;
+
+    @Bean
+    @Order(3)
+    public ApplicationRunner initPost(){
+        return args -> {
+            if(postRepository.count() == 0){
+                insertPost();
+            }
+        };
+    }
+
+    private void insertPost() {
+        List<Post> initPosts = List.of(
+                new Post(1L, "테스트", "자동배포 테스트입니다."),
+                new Post(1L, "안녕", "안녕하세요!"),
+                new Post(1L, "오늘 날짜는", "2025-09-28"),
+                new Post(1L, "title", "body!")
+        );
+
+        postRepository.saveAll(initPosts);
+    }
+}
